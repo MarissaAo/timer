e7b6cd82ad6d383c8267f4bddd0e226d320195fc
MarissaAo <marissasm.ao@gmail.com>
2026-08-29 01:14:25 +0800

    Add start and pause function

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 class Timer {
-  constructor(durationTime, startBtn, stopBtn) {
-    this.durationTime = durationTime;
+  constructor(durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput;
     this.startBtn = startBtn;
-    this.stopBtn = stopBtn;
+    this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener("click", this.start);
+    this.pauseBtn.addEventListener("click", this.pause);
   }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    console.log("tick");
+  };
 }
+
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
