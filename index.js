f95725d3ff1a54013e50a99b95829893cd7f2a8c
MarissaAo <marissasm.ao@gmail.com>
2026-08-31 22:07:28 +0800

    Complete the timer animation

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
-  onStart() {
-    console.log("Timer started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("Timer just ticked down");
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter,
+    );
   },
   onComplete() {
     console.log("Timer is completed");
